7360cab9e93f7e5975b612bfe07e2e428bf1674b
mohammedali452 <bilalmohd731@gmail.com>
2020-11-08 03:06:53 -0500

    Update Count.py

--- Count.py
+++ Count.py
@@ -1,41 +1,27 @@
+##Script that counts the number of FASTQ/FASTA files each patient has. Each patient had multiple samples so we wanted to see the range of samples each patient had.
+
 import pandas as pd
 import os
-import csv
-from matplotlib import pyplot as plt
 
-base_path = r"C:\Users\imohammed\Desktop\Microbiome\Database"
-# sample_path = os.path.join(base_path, "Sample Info.tsv")
-#
-# sample_sheet = pd.read_csv(sample_path, delimiter="\t", usecols=["subject_id"])
-# sample_count = {}
-#
-#
-# for i in sample_sheet.index:
-#     id = sample_sheet["subject_id"][i]
-#
-#     if id not in sample_count:
-#         sample_count[id] = 1
-#
-#     else:
-#         sample_count[id] += 1
-#
-# print(sample_count)
+##Folder that holds all the FASTQ/FASTA Samples. 
+base_path = r"DATA_FOLDER"
 
 database_files = os.listdir(base_path)
 sample_count = {}
 
+##Each subdirectory within base_path represents a patient
 for file in database_files:
     subject_path = os.path.join(base_path, file)
     subject_dir = os.listdir(subject_path)
 
     cnt = 0
-
+    
+    ## Counts the number of samples within a particular patient directory
     for seq in subject_dir:
         cnt += 1
 
-
-
     sample_count[file] = cnt
 
+##Prints the the number of sample each patient has
 print(sample_count)
 
